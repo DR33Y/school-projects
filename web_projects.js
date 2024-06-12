1d821f7df8f1b5188e96cd2c64ae6b341dd7a80e
DR33Y <74345638+DR33Y@users.noreply.github.com>
2024-06-12 16:27:14 +0300

    Create web_projects.js

---
+++ web_projects.js
@@ -0,0 +1,11 @@
+function togglePythonInfo() {
+    let pythonDiv = document.querySelector('#python');
+    if (pythonDiv.style.display == 'none') {
+        pythonDiv.style.display = 'block'; 
+    }
+    else
+        pythonDiv.style.display = 'none';
+}
+function init(){
+    let pythonDiv = document.querySelector('.python').style.display = 'none';
+}
