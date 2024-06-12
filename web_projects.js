561bd0efdd0b3c816e6093a13a3e3ac6e69c402f
DR33Y <74345638+DR33Y@users.noreply.github.com>
2024-06-13 02:50:58 +0300

    Update web_projects.js

--- web_projects.js
+++ web_projects.js
@@ -1,24 +1,34 @@
 function togglePythonInfo() {
     let pythonDiv = document.querySelector('#python');
-    if (document.querySelector('#js').style.display == 'none'){
-        if (pythonDiv.style.display == 'none') {
+    if (document.querySelector('#js').style.display == 'none' && document.querySelector('#java').style.display == 'none'){
+        if (pythonDiv.style.display == 'none')
             pythonDiv.style.display = 'block'; 
-        }
         else
             pythonDiv.style.display = 'none';
     }
 }
 function toggleJsInfo(){
     let jsDiv = document.querySelector('#js');
-    if(document.querySelector('#python').style.display == 'none'){
-        if (jsDiv.style.display == 'none') {
+    if (document.querySelector('#python').style.display == 'none' && document.querySelector('#java').style.display == 'none'){
+        if (jsDiv.style.display == 'none') 
             jsDiv.style.display = 'block'; 
-        }
         else
             jsDiv.style.display = 'none';
     }
 }
+function toggleJavaInfo(){
+    let javaDiv = document.querySelector('#java');
+    if (document.querySelector('#python').style.display == 'none' && document.querySelector('#js').style.display == 'none'){
+        if (javaDiv.style.display == 'none')
+            javaDiv.style.display = 'block';
+        else
+            javaDiv.style.display = 'none';
+
+    }
+
+}
 function init(){
     document.querySelector('#python').style.display = 'none';
     document.querySelector('#js').style.display = 'none';
+    document.querySelector('#java').style.display = 'none';
 }
