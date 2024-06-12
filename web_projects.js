1da4baa677c49e55f1d4c8efd1b9ea4d264a2905
DR33Y <74345638+DR33Y@users.noreply.github.com>
2024-06-12 22:44:57 +0300

    Update web_projects.js

--- web_projects.js
+++ web_projects.js
@@ -1,11 +1,24 @@
 function togglePythonInfo() {
     let pythonDiv = document.querySelector('#python');
-    if (pythonDiv.style.display == 'none') {
-        pythonDiv.style.display = 'block'; 
+    if (document.querySelector('#js').style.display == 'none'){
+        if (pythonDiv.style.display == 'none') {
+            pythonDiv.style.display = 'block'; 
+        }
+        else
+            pythonDiv.style.display = 'none';
     }
-    else
-        pythonDiv.style.display = 'none';
+}
+function toggleJsInfo(){
+    let jsDiv = document.querySelector('#js');
+    if(document.querySelector('#python').style.display == 'none'){
+        if (jsDiv.style.display == 'none') {
+            jsDiv.style.display = 'block'; 
+        }
+        else
+            jsDiv.style.display = 'none';
+    }
 }
 function init(){
-    let pythonDiv = document.querySelector('.python').style.display = 'none';
+    document.querySelector('#python').style.display = 'none';
+    document.querySelector('#js').style.display = 'none';
 }
